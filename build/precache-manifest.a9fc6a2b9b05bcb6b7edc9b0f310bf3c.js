fdb9e89b84363488d0b5040f2b4426289d817de9
Farhan Abi <farhanabiyullah@gmail.com>
2019-12-17 08:08:22 +0700

    Add frontend

---
+++ build/precache-manifest.a9fc6a2b9b05bcb6b7edc9b0f310bf3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da7ef28504aaee95b9de9894b31d81b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4eb4178e63bed15626ce",
+    "url": "/static/css/main.20c004ad.chunk.css"
+  },
+  {
+    "revision": "fcbca82d55b2cfd2a3a4",
+    "url": "/static/js/2.a34fef5f.chunk.js"
+  },
+  {
+    "revision": "4eb4178e63bed15626ce",
+    "url": "/static/js/main.04ce3ec1.chunk.js"
+  },
+  {
+    "revision": "df99deec2933845bfe71",
+    "url": "/static/js/runtime-main.b4276010.js"
+  }
+]);
